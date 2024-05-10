ae5af89073f202494c806f3391832680f10c039b
shafik720 <shafikrasel5@gmail.com>
2024-05-10 09:07:51 +0600

    fixed slick slider bug

--- src/components/For Home Page/Testimonials/TestiMonialSection.tsx
+++ src/components/For Home Page/Testimonials/TestiMonialSection.tsx
@@ -1,16 +1,25 @@
 import TestimonialCard from "./Testimonial Card/TestimonialCard";
+import Slider from "react-slick";
+import "./TestiMonialSection.css";
+import { settingsForTestimonialSliders } from "../../../utils/Slick Slider Settings/settingsForTestimonialSlider";
 
 const TestiMonialSection = () => {
   return (
     <div>
-      <h2 className="text-4xl font-bold text-center mt-20">
-        View Our Supplies
+      <h2 className="text-4xl text-[#2A4D5E] font-bold text-center mt-20">
+        Testimonials
       </h2>
       <hr className="border-1 w-3/4 mx-auto my-5" />
-      <div className="py-5 grid md:grid-cols-3 gap-6 px-5 ">
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
+      <div>
+        <Slider {...settingsForTestimonialSliders}>
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+          <TestimonialCard />
+        </Slider>
       </div>
     </div>
   );
---
+++ src/utils/Slick Slider Settings/settingsForTestimonialSlider.tsx
@@ -0,0 +1,41 @@
+
+
+export const settingsForTestimonialSliders = {
+    dots: true,
+    // infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // centerMode : true,
+    arrows : true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+    // autoplay : true
+    
+  };

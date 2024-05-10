23a2c9b66e36590b519616246af3c01deb0050e6
shafik720 <shafikrasel5@gmail.com>
2024-05-10 08:05:31 +0600

    making testimonials here

--- src/components/For Home Page/SupplySection/SupplySection.tsx
+++ src/components/For Home Page/SupplySection/SupplySection.tsx
@@ -1,17 +1,20 @@
 import SupplyCard from "./SupplyCard/SupplyCard";
 
-
 const SupplySection = () => {
-    return (
-        <div className="py-5 grid md:grid-cols-3 gap-6 px-5 ">
-            <SupplyCard />
-            <SupplyCard />
-            <SupplyCard />
-            <SupplyCard />
-            <SupplyCard />
-            <SupplyCard />
-        </div>
-    );
+  return (
+    <div>
+        <h2 className="text-4xl font-bold text-center mt-20">View Our Supplies</h2>
+        <hr className="border-2 w-3/4 mx-auto my-5" />
+      <div className="py-5 grid md:grid-cols-3 gap-6 px-5 ">
+        <SupplyCard />
+        <SupplyCard />
+        <SupplyCard />
+        <SupplyCard />
+        <SupplyCard />
+        <SupplyCard />
+      </div>
+    </div>
+  );
 };
 
 export default SupplySection;
--- src/components/For Home Page/Testimonials/TestiMonialSection.tsx
+++ src/components/For Home Page/Testimonials/TestiMonialSection.tsx
@@ -1,13 +1,19 @@
 import TestimonialCard from "./Testimonial Card/TestimonialCard";
 
 const TestiMonialSection = () => {
-    return (
-        <div  className="py-5 grid md:grid-cols-3 gap-6 px-5 ">
-            <TestimonialCard /> 
-            <TestimonialCard /> 
-            <TestimonialCard /> 
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-center mt-20">
+        View Our Supplies
+      </h2>
+      <hr className="border-1 w-3/4 mx-auto my-5" />
+      <div className="py-5 grid md:grid-cols-3 gap-6 px-5 ">
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+      </div>
+    </div>
+  );
 };
 
 export default TestiMonialSection;

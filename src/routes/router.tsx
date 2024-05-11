e5672091e96c180a652f9f0d340b977c6ca6cefc
shafik720 <shafikrasel5@gmail.com>
2024-05-11 06:22:22 +0600

    setting up react router dom

---
+++ src/routes/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+
+
+export const router = createBrowserRouter([
+    {
+        path : '/' , 
+        element : <Home />,
+        children : [
+            {
+                path : 'home',
+                element : <Home />
+            }
+        ]
+    }
+])
